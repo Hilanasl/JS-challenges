0c35b851f4414ff56ee9746100d9abda84feb064
Hilana <hilana.sene@gmail.com>
2022-11-14 18:49:02 +0100

    function created

---
+++ formating with arrays.js
@@ -0,0 +1,14 @@
+//write a function that takes an array of names and returns a message formatted
+//according to the console.log.
+
+function myFunction(a) {}
+
+console.log(myFunction([]));
+
+console.log(myFunction(["Peter"]));
+
+console.log(myFunction(["Jacob", "Alex"]));
+
+console.log(myFunction(["Max", "John", "Marc"]));
+
+console.log(myFunction(["Alex", "Jacob", "Mark", "Max"]));
