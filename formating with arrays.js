042b00fb0ce25d6c461a8801746cbf04078b757b
Hilana <hilana.sene@gmail.com>
2022-11-16 19:32:30 +0100

    solution found

--- formating with arrays.js
+++ formating with arrays.js
@@ -1,7 +1,15 @@
 //write a function that takes an array of names and returns a message formatted
 //according to the console.log.
 
-function myFunction(a) {}
+function myFunction(a) {
+  if (!a.length) {
+    return `Nobody liked it yet`;
+  } else if (a.length === 1) return `${a[0]} likes this`;
+  else if (a.length === 2) return `${a[0]} and ${a[1]} liked this`;
+  else if (a.length === 3) return `${a[0]}, ${a[1]} and ${a[2]} liked this`;
+  else a.length > 3;
+  return `${a[0]}, ${a[1]} and ${a.length - 2} others liked this`;
+}
 
 console.log(myFunction([]));
 
